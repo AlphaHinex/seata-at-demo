67e5bb25149b251bd35363a934a170b82d9bf357
Alpha Hinex <AlphaHinex@gmail.com>
2019-12-23 15:01:06 +0800

    Add maven project

---
+++ storage/src/main/java/io/github/alphahinex/demo/seataat/storage/StorageApplication.java
@@ -0,0 +1,13 @@
+package io.github.alphahinex.demo.seataat.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageApplication.class, args);
+	}
+
+}
