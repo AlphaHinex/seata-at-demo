fa33827ac19e6d50d4cc6ef0748668cb6919475b
Alpha Hinex <AlphaHinex@gmail.com>
2020-01-02 13:49:42 +0800

    Clean up

--- modules/storage/src/test/groovy/io/github/alphahinex/demo/seata/at/storage/StorageApplicationTest.groovy
+++ modules/storage/src/test/groovy/io/github/alphahinex/demo/seata/at/storage/StorageApplicationTest.groovy
@@ -1,28 +1,28 @@
-package io.github.alphahinex.demo.seata.at.storage;
+package io.github.alphahinex.demo.seata.at.storage
 
-import groovy.json.JsonOutput;
-import io.github.alphahinex.demo.seata.at.storage.vo.CommodityVO;
-import io.github.springroll.test.AbstractSpringTest;
-import org.junit.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
+import groovy.json.JsonOutput
+import io.github.alphahinex.demo.seata.at.storage.vo.CommodityVO
+import io.github.springroll.test.AbstractSpringTest
+import org.junit.Test
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.transaction.annotation.Transactional
 
 @Sql
 @Transactional
 class StorageApplicationTest extends AbstractSpringTest {
 
-	@Test
-	void contextLoads() {
-		String code = "test";
-		get("/at/storage/" + code, HttpStatus.OK);
+    @Test
+    void contextLoads() {
+        String code = "test"
+        get("/at/storage/" + code, HttpStatus.OK)
 
-		CommodityVO vo = new CommodityVO();
-		vo.setCount(2);
-		vo.setCommodityCode(code);
-		put("/at/storage/decrease", JsonOutput.toJson(vo), HttpStatus.OK);
+        CommodityVO vo = new CommodityVO()
+        vo.setCount(2)
+        vo.setCommodityCode(code)
+        put("/at/storage/decrease", JsonOutput.toJson(vo), HttpStatus.OK)
 
-		get("/at/storage/" + code, HttpStatus.OK);
-	}
+        get("/at/storage/" + code, HttpStatus.OK)
+    }
 
 }
