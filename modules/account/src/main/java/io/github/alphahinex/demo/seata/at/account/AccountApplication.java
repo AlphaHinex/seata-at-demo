e8fb8b0dbb03cbf8dcd051562d8adf3f60b38ae1
Alpha Hinex <AlphaHinex@gmail.com>
2020-01-08 15:10:32 +0800

    Depends on all 3 jars

--- modules/account/src/main/java/io/github/alphahinex/demo/seata/at/account/AccountApplication.java
+++ modules/account/src/main/java/io/github/alphahinex/demo/seata/at/account/AccountApplication.java
@@ -1,11 +1,12 @@
 package io.github.alphahinex.demo.seata.at.account;
 
+import com.alibaba.cloud.seata.GlobalTransactionAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = GlobalTransactionAutoConfiguration.class)
 @EnableDiscoveryClient
 @ImportResource("classpath:/initializeDatabase.xml")
 public class AccountApplication {
--- modules/order/src/main/java/io/github/alphahinex/demo/seata/at/order/OrderApplication.java
+++ modules/order/src/main/java/io/github/alphahinex/demo/seata/at/order/OrderApplication.java
@@ -1,11 +1,12 @@
 package io.github.alphahinex.demo.seata.at.order;
 
+import com.alibaba.cloud.seata.GlobalTransactionAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = GlobalTransactionAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 public class OrderApplication {
